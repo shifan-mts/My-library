5d4d72ea37d7faca62a7b4c0fe2657a8e9526b5c
shifan-mts <amohamedshifan1@gmail.com>
2025-01-05 13:03:24 +0530

    developed the popup page

---
+++ script.js
@@ -0,0 +1,9 @@
+const s = document.querySelector(".input-btn");
+const popup = document.querySelector(".popcontainer");
+const submit = document.querySelector(".submit2");
+s.addEventListener("click",()=>{
+    popup.style= "display:block;";
+});
+submit.addEventListener("click",()=>{
+    popup.style= "display:block;";
+});
